579ccbcb2d86665fc0f979282978ef5c82667a79
Jacob Spade <spade.jacob@yahoo.com>
2018-11-17 20:54:51 -0800

    added redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import {Provider} from 'react-redux';
+import store from './store';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
